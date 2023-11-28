92e738c2e69881691c7f7768269d0f1b7d258ab3
FreddieCasa <lambada.is.agurkas@gmail.com>
2023-11-28 13:55:25 +0200

    BookCatalogImpl added

--- src/Testams.java
+++ src/Testams.java
@@ -1,2 +1,18 @@
-public class Testams {
+import lt.techin.library.Book;
+import lt.techin.library.BookCatalog;
+import lt.techin.library.test.AbstractLibraryTest;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Predicate;
+
+public class Testams extends AbstractLibraryTest {
+
+
+
+    @Override
+    protected BookCatalog createBookCatalog() {
+        return new BookCatalogImpl();
+    }
 }
